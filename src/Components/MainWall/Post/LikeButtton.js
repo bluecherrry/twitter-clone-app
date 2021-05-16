8ebc24654723349752661680f2b39756fee699d8
bluecherrry <shararehshadlo@gmail.com>
2021-05-16 20:52:30 +0430

    like and dislike button done

--- src/Components/MainWall/Post/LikeButtton.js
+++ src/Components/MainWall/Post/LikeButtton.js
@@ -11,23 +11,19 @@ Parse.serverURL = 'http://localhost:1337/parse'
 function LikeButtton({ post }) {
     const [state, dispatch] = useReducer(LikeReducer, initialState);
     const { likes, active } = state;
-   
     const user = Parse.User.current();
     const PostsLike = Parse.Object.extend("PostsLike");
     const postsLike = new PostsLike();
     postsLike.set("user", user);
     postsLike.set("post", post);
-
-
     const saveLike = () => {
         let postId = postsLike.get('post').id
         console.log(postId);
         postsLike.save()
             .then((res) => {
-                
                 dispatch({
                     type: HANDLE_LIKE,
-                    payload : res
+                    payload: res
                 })
             })
     }
@@ -35,28 +31,27 @@ function LikeButtton({ post }) {
     const disLike = async () => {
         const query = new Parse.Query(PostsLike);
         query.equalTo("post", post);
-        const results = await query.find()
-       
-        
-            dispatch({
-            type:HANDLE_DISLIKE,payload:{Likes : results, UserId : user.id}
-                
+        const results = await query.find().then((u) => {
+            let d = u.find(e => e.attributes.user.id === user.id)
+            d.destroy().then((e) => {
+                dispatch({
+                    type: HANDLE_DISLIKE, payload: { Likes: u, UserId: user.id }
+
+                })
+            }
+
+            )
             
-         
         })
-       
     }
 
     const showLikes = async () => {
         const query = new Parse.Query(PostsLike);
         query.equalTo("post", post);
         const results = await query.find();
-       
         dispatch({
-            type: 'init_like', payload: {likes : results, userId : user.id}
+            type: 'init_like', payload: { likes: results, userId: user.id }
         })
-
-
     }
 
 
@@ -69,12 +64,12 @@ function LikeButtton({ post }) {
         <div style={{ display: "flex" }} >
             <Button
                 style={{ border: "none" }}
-               
+
                 onClick={() =>
                     active ? disLike() : saveLike()
                 }
             >
-                {active  ? <HeartTwoTone twoToneColor="#eb2f96" /> : <HeartOutlined />}
+                {active ? <HeartTwoTone twoToneColor="#eb2f96" /> : <HeartOutlined />}
                 <br />
                 {likes.length}
 
